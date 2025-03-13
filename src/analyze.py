f9dfb5be3e9b15c1ffc66b7493b342aa889e792b
kkamo22 <kkamo.std22@keio.jp>
2025-03-13 15:59:37 +0900

    Create analyze.py

---
+++ src/analyze.py
@@ -0,0 +1,48 @@
+import pathlib
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from modules.fileio import read_csv
+
+
+BITS = 10
+VCC = 3.3
+GAIN = 1009
+
+
+def calc_emg(data):
+    emg = ((np.array(data) / 2**BITS) - 1/2) * VCC * 1000 / GAIN
+    return emg
+
+
+def calc_ema(arr, rho):
+    ems = [arr[0]]
+    for val in arr[1:]:
+        ems.append(rho*ems[-1] + (1 - rho)*val)
+    return np.array(ems)
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    label, data = read_csv(pathlib.Path(filename).resolve())
+
+    emg = calc_emg(data)
+
+    # EMS を計算
+    ema1 = calc_ema(emg, 0.9)
+    ema2 = calc_ema(emg, 0.95)
+    ema3 = calc_ema(emg, 0.99)
+
+    # グラフを表示
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(emg, alpha=0.25, label="EMG")
+    ax.plot(ema1, alpha=0.25, label="EMA (ρ = 0.9)")
+    ax.plot(ema2, alpha=0.25, label="EMA (ρ = 0.95)")
+    ax.plot(ema3, alpha=0.25, label="EMA (ρ = 0.99)")
+    ax.legend()
+    ax.set_xlabel("Index")
+    ax.set_ylabel("Output")
+    plt.show()
--- src/visualizer.py
+++ src/visualizer.py
@@ -1,29 +1,24 @@
+import pathlib
 import sys
+
 import matplotlib.pyplot as plt
-import csv
 
-
-def read_csv(filename):
-    with open(filename, newline='') as f:
-        reader = csv.reader(f)
-        label = next(reader)[0]  # 1行目のラベル
-        data = [float(row[0]) for row in reader]  # 2行目以降のデータ
-    return label, data
+from modules.fileio import read_csv
 
 
 def main():
     if len(sys.argv) != 3:
         print("Usage: python script.py <csv_file1> <csv_file2>")
         sys.exit(1)
-    
+
     file1, file2 = sys.argv[1], sys.argv[2]
-    label1, data1 = read_csv(file1)
-    label2, data2 = read_csv(file2)
-    
+    label1, data1 = read_csv(pathlib.Path(file1).resolve())
+    label2, data2 = read_csv(pathlib.Path(file2).resolve())
+
     if label1 != label2:
         print("Error: CSV files must have the same label in the first row.")
         sys.exit(1)
-    
+
     plt.plot(data1, label=f"{file1}", alpha=0.5)
     plt.plot(data2, label=f"{file2}", alpha=0.5)
     plt.xlabel("Time")
